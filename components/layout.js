665f506a107bdca72a0185556f84cc916ff757bb
Travis Horn <travis@travishorn.com>
2022-03-31 15:00:02 -0500

    SEO Metadata

--- components/layout.js
+++ components/layout.js
@@ -23,6 +23,10 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
+        <meta
+          name="og:description"
+          content="Learn how to build a personal website using Next.js"
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
         <meta name="google" content="notranslate" key="notranslate" />
